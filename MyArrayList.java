34d4b8e8d82c63a512c220468bafa426710055a2
Muntahaa Khan <103258353+Pasha-007@users.noreply.github.com>
2024-10-21 23:19:01 +0600

    Array and Array List

---
+++ MyArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MyArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> friendArrayList = 
+        new ArrayList<>(Arrays.asList("John", "Chris", "Eric"));
+        
+        System.out.println(friendArrayList);
+    }
+}
